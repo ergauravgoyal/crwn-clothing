4f770b7ab5b2c78c77b6d1affbb4300bd81adb18
ergauravgoyal <gauravgoyal129@gmail.com>
2023-10-05 19:24:20 +0530

    Product Context added

---
+++ src/contexts/product.context.js
@@ -0,0 +1,18 @@
+import React, { createContext, useState } from 'react';
+import PRODUCTS from '../shop.data.json';
+
+//as actual value we want to access
+export const ProductsContext = createContext({
+  products: [],
+});
+
+export const ProductsProvider = ({ children }) => {
+  const [products, setProducts] = useState(PRODUCTS);
+  const value = { products, setProducts };
+
+  return (
+    <ProductsContext.Provider value={value}>
+      {children}
+    </ProductsContext.Provider>
+  );
+};
