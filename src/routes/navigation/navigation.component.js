ebf03b6c1c4b07776d553095d55891cfba677d7b
ergauravgoyal <gauravgoyal129@gmail.com>
2023-10-05 21:00:12 +0530

    cart dropdown added

--- src/routes/navigation/navigation.component.js
+++ src/routes/navigation/navigation.component.js
@@ -1,9 +1,11 @@
 import React, { useContext } from 'react';
 import { Link, Outlet } from 'react-router-dom';
 import './navigation.styles.scss';
+import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
 import { UserContext } from '../../contexts/user.context';
 import { signOutUser } from '../../utils/firebase/firebase.utils';
 import CartIcon from '../../components/cart-icon/cart-icon.component';
+import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
 
 const Navigation = () => {
   const { currentUser } = useContext(UserContext);
@@ -12,11 +14,8 @@ const Navigation = () => {
     <>
       <div className="navigation">
         <Link className="logo-container" to="/">
-          <img src="https://stackblitz-starters-kxhgru.stackblitz.io/assets/crown.svg" />
-        </Link>
-        {/* <Link className="logo-container" to="/">
           <CrwnLogo className="logo" />
-        </Link> */}
+        </Link>
         <div className="nav-links-container">
           <Link className="nav-link" to="/shop">
             SHOP
@@ -32,6 +31,7 @@ const Navigation = () => {
           )}
           <CartIcon />
         </div>
+        <CartDropdown />
       </div>
       <Outlet />
     </>
